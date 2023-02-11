417ed509dbe94f9257c8a531dd3e3d97e2016b93
Chiran73 <Chiranjeevi.Lokeshwaren@rencata.com>
2023-02-11 06:44:49 -0800

    Document Save Completed

--- University Admin Web/university_admin_web/src/Components/Document.js
+++ University Admin Web/university_admin_web/src/Components/Document.js
@@ -1,25 +1,27 @@
 import React, {useState, useEffect} from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { GrView } from 'react-icons/gr';
 import { FaFileDownload } from 'react-icons/fa';
-import { useNavigate, Link } from "react-router-dom";
 import Table from 'react-bootstrap/Table';
-import Modal from 'react-bootstrap/Modal';
+import toast from 'react-simple-toasts';
 
 function Document(){
+    const [studentName, setStudentName] = useState("Jim Carey");
+    const [registrationNumber, setRegistrationNumber] = useState(null);
     const [modalShow, setModalShow] = React.useState(false);
     const [gridData, setGridData] = useState([]);
     const [documentType, setDocumentType] = useState(null);
-    const gridVals = [{id: 1, documentType: "Professional Certificate" }]
+    const [filePath, setFilePath] = useState(null);
 
     const handleInputChange = (e) => {
+        debugger;
         const {id , value} = e.target;
         if(id === "documentType")
             setDocumentType(value);
+        if(id === "filePath")
+            setFilePath(e.target.files[0].name);
     }
 
     const getDocumentGridData  = (e) => {
-        fetch('https://localhost:44342/api/Student/GetStudents', 
+        fetch('https://localhost:44343/api/Document/GetDocument', 
         { 
             method: 'GET',
             withCredentials: true, 
@@ -30,6 +32,7 @@ function Document(){
         }) 
         .then((res) => res.json())
         .then((data) => {
+            setRegistrationNumber(data[0].registrationNo)
             setGridData(data);
             console.log(data);
         })
@@ -39,78 +42,85 @@ function Document(){
     }
 
     useEffect(() => {
-        //getDocumentGridData();
-        setGridData(gridVals);
+        getDocumentGridData();
      }, [])
 
      const handleSaveSubmit = (e) => {
-        
+        const postBody = {
+            RegistrationNo: registrationNumber,
+            DocumentType: documentType,
+            FilePath: filePath
+        };
+        fetch('https://localhost:44343/api/Document/CreateDocument', 
+        { 
+            method: 'POST',
+            body: JSON.stringify(postBody),
+            withCredentials: true, 
+            crossorigin: true,
+            headers: {
+            Accept: 'application/json','Content-Type': 'application/json'
+            },
+        }) 
+        .then((res) => res.json())
+        .then((data) => {
+            getDocumentGridData();
+            toast(<><b style={{ color: 'Green' }}>{data}</b></>, { position: 'top-right' });
+            console.log(data);
+        })
+        .catch((error) => {
+            toast(<><b style={{ color: 'Red' }}>{error}</b></>, { position: 'top-right' });
+            console.error(error);
+        });
      }
-
-     function AddDocumentModal(props) {
-        return (
-          <Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
-            <Modal.Header closeButton>
-              <Modal.Title id="contained-modal-title-vcenter">
-                Add Document
-              </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <div className='row'>
-                <div className='col-md-6'>
-                        <label className="form_label" for="documentType">Document Type </label><br></br>
-                        <select className="form_input" id="documentType" value={documentType} onChange = {(e) => handleInputChange(e)}>
-                            <option value={0}>-Select-</option>
-                            <option value={1}>Degree Certificate</option>
-                            <option value={2}>Provisional Certificate</option>
-                            <option value={3}>Consolidated Marks Sheet</option>
-                        </select>
-                </div>
-                <div className='col-md-6' style={{paddingTop: "25px"}}>
-                    <input  type="file" name="" id="documentBrowse"/>
-                </div>
-              </div>
-            </Modal.Body>
-            <Modal.Footer>
-                <button variant="primary" onClick={(e)=>handleSaveSubmit(e)} type="submit" class="btn_add_document_save">Save</button>
-                <button variant="primary" onClick={props.onHide} type="submit" class="btn_add_document_close">Close</button>
-            </Modal.Footer>
-          </Modal>
-        );
-      }
     
     return(
         <div className="form-document">
             <div><h1 className='document_header'>Documents</h1></div>
             <div className="form-document-body">
-            <span style={{paddingLeft: "87%"}}></span>
-            <button variant="primary" onClick={() => setModalShow(true)} type="submit" class="btn_add_document">Add Document</button>
-            <AddDocumentModal show={modalShow} onHide={() => setModalShow(false)}/>
+            <div className='row'>
+                <div className='col-md-6'>
+                    <label className="studentName_label" for="studentName">Student :&nbsp;</label>
+                    <label className="studentName_label" type="number" id="studentName">{studentName}</label>
+                </div>
+                <div className='col-md-6'>
+                    <label className="registrationNumber_label" for="registrationNumber">Registration Number :&nbsp;</label>
+                    <label className="registrationNumber_label" type="number" id="registrationNumber">{registrationNumber}</label>
+                </div>
+            </div>
+            <br></br>
+            <div className='row' style={{paddingTop: "50px"}}>
+                <div className='col-md-4'>
+                        <label className="form_label" for="documentType">Document Type </label><br></br>
+                        <select className="form_input" id="documentType" value={documentType} onChange = {(e) => handleInputChange(e)}>
+                            <option value={"0"}>-Select-</option>
+                            <option value={"Degree Certificate"}>Degree Certificate</option>
+                            <option value={"Provisional Certificate"}>Provisional Certificate</option>
+                            <option value={"Consolidated Marks Sheet"}>Consolidated Marks Sheet</option>
+                        </select>
+                </div>
+                <div className='col-md-4' style={{paddingTop: "25px"}}>
+                    <input  type="file" name="" id="filePath" onChange = {(e) => handleInputChange(e)}/>
+                </div>
+                <div className='col-md-4' style={{paddingTop: "25px"}}>
+                    <button variant="primary" onClick={(e)=>handleSaveSubmit(e)} type="submit" class="btn_add_document">Save Document</button>
+                </div>
+              </div>
+            
             <Table responsive bordered hover>
                 <thead>
                     <tr>
-                        <th>SNo.</th>
                         <th>Document Type</th>
+                        <th>File Path</th>
                         <th>Action</th>
                     </tr>
                 </thead>
                 <tbody>
                     {gridData.map(d =>
                         <tr key = {d.id}>
-                            <td>{d.id}</td>
                             <td>{d.documentType}</td>
+                            <td>{d.filePath}</td>
                             <td>
-                                <div className='row'>
-                                    <div className='col-sm-2'>
-                                        <h3 style={{cursor:'pointer'}}><GrView/></h3>
-                                    </div>
-                                    <div className='col-sm-2'>
-                                        <h3 style={{cursor:'pointer'}}><FaFileDownload/></h3>
-                                    </div>
-                                    <div className='col-sm-8'>
-
-                                    </div>
-                                </div>
+                                <span style={{fontSize: "large", cursor: 'pointer', color: '#785fa0'}}><FaFileDownload /> Download</span>
                             </td>
                         </tr>
                     )}
