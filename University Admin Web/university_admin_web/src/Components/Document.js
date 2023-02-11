f41743afd0747812552234fb3178166c9514a3c0
muthu1986 <muthukumar.balakrishnan@rencata.com>
2023-02-11 22:16:40 +0530

    UI changes

--- University Admin Web/university_admin_web/src/Components/Document.js
+++ University Admin Web/university_admin_web/src/Components/Document.js
@@ -5,12 +5,14 @@ import { FaFileDownload } from 'react-icons/fa';
 import { useNavigate, Link } from "react-router-dom";
 import Table from 'react-bootstrap/Table';
 import Modal from 'react-bootstrap/Modal';
+import FileBase64 from 'react-file-base64';
 
 function Document(){
     const [modalShow, setModalShow] = React.useState(false);
     const [gridData, setGridData] = useState([]);
     const [documentType, setDocumentType] = useState(null);
     const gridVals = [{id: 1, documentType: "Professional Certificate" }]
+    const [item, setItem] = useState({ title: '', image: '' });
 
     const handleInputChange = (e) => {
         const {id , value} = e.target;
@@ -39,12 +41,96 @@ function Document(){
     }
 
     useEffect(() => {
-        //getDocumentGridData();
         setGridData(gridVals);
      }, [])
 
+    
+
+    //  function processFile(e) {
+    //    var size = file.size;
+    //    var sliceSize = 20000000;
+    //    var start = 0;
+ 
+    //    setTimeout(loop, 3);
+ 
+    //    function loop() {
+    //      var end = start + sliceSize;
+ 
+    //      if (end > size) {
+    //        end = size;
+    //      }
+    //      var s = slice(file, start, end);
+    //      send(s, start, end - 1, size);
+    //      if (end < size) {
+    //        start += sliceSize;
+    //        setTimeout(loop, 3);
+    //      }
+    //    }
+    //  }
+ 
+    //  function send(piece, start, end, size) {
+    //    console.log("start ", start);
+    //    console.log("end", end);
+ 
+    //    var formdata = new FormData();
+    //    console.log(XUniqueUploadId);
+ 
+    //    formdata.append("file", piece);
+    //    formdata.append("cloud_name", YOUR_CLOUD_NAME);
+    //    formdata.append("upload_preset", YOUR_UNSIGNED_UPLOAD_PRESET);
+    //    formdata.append("public_id", "myChunkedFile2");
+ 
+    //    var xhr = new XMLHttpRequest();
+    //    xhr.open("POST", POST_URL, false);
+    //    xhr.setRequestHeader("X-Unique-Upload-Id", XUniqueUploadId);
+    //    xhr.setRequestHeader(
+    //      "Content-Range",
+    //      "bytes " + start + "-" + end + "/" + size
+    //    );
+ 
+    //    xhr.onload = function () {
+    //      // do something to response
+    //      console.log(this.responseText);
+    //    };
+ 
+    //    xhr.send(formdata);
+    //  }
+ 
+    //  function slice(file, start, end) {
+    //    var slice = file.mozSlice
+    //      ? file.mozSlice
+    //      : file.webkitSlice
+    //      ? file.webkitSlice
+    //      : file.slice
+    //      ? file.slice
+    //      : noop;
+ 
+    //    return slice.bind(file)(start, end);
+    //  }
+
      const handleSaveSubmit = (e) => {
-        
+        debugger;
+        const postBody = {
+            RegistrationNo: "FC10001",
+            DocumentType: documentType,
+            FilePath: item.image,
+        };
+
+        //processFile(e);
+
+        fetch('https://localhost:44343/api/Document/CreateDocument', 
+        { 
+            method: 'POST',
+            body: JSON.stringify(postBody),
+            withCredentials: true, 
+            crossorigin: true,
+            headers: {
+            Accept: 'application/json','Content-Type': 'application/json'
+            },
+        }) 
+        .then((res) => res.json())
+        .catch((error) => {
+        });
      }
 
      function AddDocumentModal(props) {
@@ -68,6 +154,7 @@ function Document(){
                 </div>
                 <div className='col-md-6' style={{paddingTop: "25px"}}>
                     <input  type="file" name="" id="documentBrowse"/>
+                   
                 </div>
               </div>
             </Modal.Body>
@@ -83,8 +170,11 @@ function Document(){
         <div className="form-document">
             <div><h1 className='document_header'>Documents</h1></div>
             <div className="form-document-body">
-            <span style={{paddingLeft: "87%"}}></span>
-            <button variant="primary" onClick={() => setModalShow(true)} type="submit" class="btn_add_document">Add Document</button>
+            
+            <input type="text" onChange={e => setItem({ ...item, title: e.target.value })}/>
+            <FileBase64 type="file" multiple={false} onDone={({ base64 }) => setItem({ ...item, image: base64 })}/>
+            {/* <button variant="primary" onClick={(e)=>ApproveCollege(e)}>Add Document</button> */}
+            {/* <button variant="primary" onClick={() => setModalShow(true)} type="submit" class="btn_add_document">Add Document</button> */}
             <AddDocumentModal show={modalShow} onHide={() => setModalShow(false)}/>
             <Table responsive bordered hover>
                 <thead>
